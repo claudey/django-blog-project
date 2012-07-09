326457ce05f51eaf15116a6ed18eea0e5065ba85
claudey <clauditey@gmail.com>
2012-07-09 15:17:08 +0000

    Steadily progressing thru the blog app

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,10 @@ from django.contrib import admin
 class Post(models.Model):
 	title = models.CharField(max_length=60)
 	body = models.TextField()
-	created = models.DateField()
-	updated = models.DateField()
+	author = models.CharField(max_length=60)
+	post = models.CharField(max_length=60)
+	created = models.DateField(auto_now = True, auto_now_add = True)
+	updated = models.DateField(auto_now = True, auto_now_add = True)
 		
 	def __unicode__(self):
 		return self.title
@@ -14,12 +16,30 @@ class Post(models.Model):
 class Comment(models.Model):
 	body = models.TextField()
 	author = models.CharField(max_length=60)
-	created = models.DateField()
-	updated = models.DateField()
+	created = models.DateField(auto_now = True, auto_now_add = True)
+	updated = models.DateField(auto_now = True, auto_now_add = True)
 	post = models.ForeignKey(Post)
+	list_display = ('title_first_60')
 	
 	def __unicode__(self):
 		return self.body
+		
+class CommentInline(admin.TabularInline):
+	model = Comment
+		
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'created', 'updated')
+	search_fields = ('title', 'body')
+	list_filter = ('created', 'author')
+	ordering = ('title', 'post')
+	inlines = [CommentInline]
 	
-admin.site.register(Post)
-admin.site.register(Comment)
+	def __unicode__(self):
+		return self.title
+		
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('post', 'author')
+	list_filter = ('created', 'author')
+	
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+
+from models import Post, Comment 
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    
+    print type(post_list)
+    print post_list
+    
+    return HttpResponse('This should be a list of posts!')
+
+def post_detail(request, id, showComments=False):
+    pass
+    
+def post_search(request, term):
+    pass
+
+def home(request):
+    print 'it works'
+    return HttpResponse('hello world. Ete zene?') 
